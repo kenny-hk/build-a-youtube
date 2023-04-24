acbfb5b39fe8f05da6c01318a25db02230770eb7
kenny-hk <112651641+kenny-hk@users.noreply.github.com>
2023-04-24 16:51:32 -0400

    Initial commit

--- app.py
+++ app.py
@@ -1,35 +1,74 @@
 import os
-
+import csv
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
+MODEL_NAME = "text-davinci-003"
+
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
+DATASET_PATH = "./data/CAvideos_20_entries.csv"
+
+# Read CSV into list of lists
+videos = []
+with open(DATASET_PATH, 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    headers = next(reader)  # extract headers as first row
+    # extract only the useful columns
+    for row in reader:
+        video = {"title": row[headers.index("title")], "thumbnail_link": row[headers.index("thumbnail_link")], "tags": row[headers.index("tags")], "views": row[headers.index("views")], "likes": row[headers.index("likes")]}
+        videos.append(video)
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+        
+        #Get criteria from users' input
+        criteria = request.form["criteria"]
+        
+        # Apply rate function to each row's title and store the summary in a new list
+        
+        rate (videos, criteria)
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+        #Sort the data by criteria in descending order
+        sorted_data = sorted(videos, key=lambda x: float(x["rating"]), reverse=True)
+        
+        # Keep only the top 5 videos
+        sorted_data = sorted_data[:5]
 
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+        return render_template("index.html", result=sorted_data)   
+    
+    return render_template("index.html")
+    
+#rate function    
+def rate(videos, criteria):
+    if any(substring in criteria for substring in ["like", "engagement", "view"]):
+        prompt = f'Rate the following videos with these title, tags, views, and likes by the following criteria - {criteria} - from 0 to 1 in 2 decimal points and return only their ratings as a list of 2 decimal point floats. Assume that a video with a higher like count should receive a higher rating:\n\n'
+    else:
+        prompt = f'Rate the following videos with these title, tags, views, and likes by the following criteria - {criteria} - from 0 to 1 in 2 decimal points and return only their ratings as a list of 2 decimal point floats. The more the video title and tags match with the criteria, the higher the rating:\n\n'
+    for i, video in enumerate(videos):
+        prompt += f"{i+1}: "
+        prompt += f"{video['title']}"
+        prompt += f"Tags: {video['tags']}"
+        prompt += f" Like count: {video['likes']} "
+        prompt += f"Views: {video['views']}\n"
+    print(prompt)
+    response = openai.Completion.create(
+        model=MODEL_NAME,
+        prompt=prompt,
+        temperature=0.1,
+        max_tokens=500,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
+    ratings = response.choices[0].text.strip()
+    ratings = ratings.strip("[")
+    ratings = ratings.strip("]")
+    ratings_list = [float(rating) for rating in ratings.split(", ")]
+    print(ratings_list)
+    for i, rating in enumerate(ratings_list):
+        videos[i]["rating"] = rating
+    
+    return videos
